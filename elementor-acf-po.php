8bd19be9cd4dbd4c51ebbd90c3405897fe63389b
Ben Owen <circleb@users.noreply.github.com>
2021-03-17 20:58:49 -0500

    Initial Commit

---
+++ elementor-acf-po.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Plugin Name:       ACF Post Object Elementor List Widget
+ * Plugin URI:        benowen.net
+ * Description:       Adds the ability to display the contents of an ACF Post Object field as a list of links.
+ * Version:           1.0.0
+ * Author:            Ben Owen
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+final class Elementor_ACF_PO_Extension {
+	const VERSION = '1.0.0';
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+	const MINIMUM_PHP_VERSION = '7.0';
+
+	private static $_instance = null;
+
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	public function __construct() {
+		add_action( 'plugins_loaded', [ $this, 'on_plugins_loaded' ] );
+	}
+
+	public function i18n() {
+		load_plugin_textdomain( 'elementor-acf-po-li-extension' );
+	}
+
+	public function on_plugins_loaded() {
+		if ( $this->is_compatible() ) {
+			add_action( 'elementor/init', [ $this, 'init' ] );
+		}
+	}
+
+	public function is_compatible() {
+
+		// Check if Elementor is installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
+			return false;
+		}
+
+		// Check if ACF is installed and activated
+		if ( ! class_exists('ACF') ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_acf_plugin' ] );
+			return false;
+		}
+
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_elementor_version' ] );
+			return false;
+		}
+
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
+			return false;
+		}
+
+		return true;
+
+	}
+
+	public function init() {
+	
+		$this->i18n();
+
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
+
+	}
+
+	public function init_widgets() {
+
+		// Include Widget files
+		require_once( __DIR__ . '/widget.php' );
+
+		// Register widget
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Elementor_ACF_PO_Widget() );
+
+	}
+
+	public function admin_notice_missing_main_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'elementor-acf-po-li-extension' ),
+			'<strong>' . esc_html__( 'Elementor ACF PO Extension', 'elementor-acf-po-li-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-acf-po-li-extension' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	public function admin_notice_missing_acf_plugin() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" or "%3$s" to be installed and activated.', 'elementor-acf-po-li-extension' ),
+			'<strong>' . esc_html__( 'Elementor ACF PO Extension', 'elementor-acf-po-li-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Advanced Custom Fields', 'elementor-acf-po-li-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Advanced Custom Fields PRO', 'elementor-acf-po-li-extension' ) . '</strong>'
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	public function admin_notice_minimum_elementor_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-acf-po-li-extension' ),
+			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementor-acf-po-li-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'elementor-acf-po-li-extension' ) . '</strong>',
+			 self::MINIMUM_ELEMENTOR_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+	public function admin_notice_minimum_php_version() {
+
+		if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
+
+		$message = sprintf(
+			/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'elementor-acf-po-li-extension' ),
+			'<strong>' . esc_html__( 'Elementor Test Extension', 'elementor-acf-po-li-extension' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'elementor-acf-po-li-extension' ) . '</strong>',
+			 self::MINIMUM_PHP_VERSION
+		);
+
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+
+	}
+
+}
+
+Elementor_ACF_PO_Extension::instance();
---
+++ widget.php
@@ -0,0 +1,165 @@
+<?php
+class Elementor_ACF_PO_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'acf_po';
+	}
+
+	public function get_title() {
+		return __( 'ACF Post Object List', 'elementor-acf-po-li-extension' );
+	}
+
+	public function get_icon() {
+		return 'eicon-bullet-list';
+	}
+
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	protected function _register_controls() {
+
+		$this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Post Object', 'elementor-acf-po-li-extension' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				'label' => __( 'Title', 'elementor-acf-po-li-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'placeholder' => __( 'Title Here', 'elementor-acf-po-li-extension' ),
+			]
+		);
+
+		$this->add_control(
+			'field',
+			[
+				'label' => __( 'ACF Field Slug', 'elementor-acf-po-li-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'placeholder' => __( 'post_object', 'elementor-acf-po-li-extension' ),
+				'description' => __( 'This is the "Name" you assigned to your Post Object field in ACF.' ),
+			]
+		);
+
+		$this->add_control(
+			'seperator',
+			[
+				'label' => __( 'Seperator', 'elementor-acf-po-li-extension' ),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'placeholder' => __( 'e.g. , | <br>', 'elementor-acf-po-li-extension' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => __( 'Post Object', 'elementor-acf-po-li-extension' ),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'title_color',
+			[
+				'label' => __( 'Color', 'elementor-acf-po-li-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Scheme_Color::get_type(),
+					'value' => \Elementor\Scheme_Color::get_default_scheme(),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .acf-po-elementor-title' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'title_typography',
+				'label' => __( 'Typography', 'elementor-acf-po-li-extension' ),
+				'scheme' => \Elementor\Scheme_Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .acf-po-elementor-title',
+			]
+		);
+
+		$this->add_control(
+			'posts_color',
+			[
+				'label' => __( 'Color', 'elementor-acf-po-li-extension' ),
+				'type' => \Elementor\Controls_Manager::COLOR,
+				'scheme' => [
+					'type' => \Elementor\Scheme_Color::get_type(),
+					'value' => \Elementor\Scheme_Color::get_default_scheme(),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .acf-po-elementor-posts' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name' => 'post_typography',
+				'label' => __( 'Typography', 'elementor-acf-po-li-extension' ),
+				'scheme' => \Elementor\Scheme_Typography::TYPOGRAPHY_1,
+				'selector' => '{{WRAPPER}} .acf-po-elementor-posts',
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	protected function render() {
+
+		$settings = $this->get_settings_for_display();
+		$title = $settings['title'];
+		$field = $settings['field'];
+		$seperator = $settings['seperator'];
+		$posts = get_field($field);
+		$html = '';
+
+		if( $posts ) {
+
+			$html .= '<span class="acf-po-elementor-title">'.$title.'</span>';
+
+			if (is_object($posts)) {
+
+				$html .= '<a class="acf-po-elementor-posts" href="'. $posts->guid . '">' . $posts->post_title . '</a>';
+
+			} else if (is_array($posts)) {
+
+				$count = count($posts);
+
+				foreach ($posts as $post) {
+
+					$html .= '<a class="acf-po-elementor-posts" href="'. $post->guid . '">' . $post->post_title . '</a>';
+
+				    if (--$count > 0) {
+
+				    	$html .= $seperator;
+
+				    }
+
+				}
+
+			}
+
+		}
+
+		echo ( $html );
+
+	}
+
+}
